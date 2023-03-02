3fdb3e966ce16c1bd26069b0e035e3b5b960aec3
Sebastian Camilo Vanegas Ayala <scva9708@gmail.com>
2023-03-02 11:53:19 -0500

    Initial commit

---
+++ app/src/main/java/moviles20231/layouts/MainActivity.java
@@ -0,0 +1,14 @@
+package moviles20231.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
